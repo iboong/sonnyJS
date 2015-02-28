b7cc57410800e682f2b3327185a48324592a3443
Pavel Buzdin <pbuzdin@gmail.com>
2015-03-01 02:50:37 +0300

    vse ploho

--- test/includes/script.js
+++
@@ -1,8 +0,0 @@
-(function() {
-
-    var instance = new SONNY.Instance(function() {
-        var renderer = new SONNY.Renderer(instance);
-            renderer.render("public/index.html");
-    });
-
-})();
